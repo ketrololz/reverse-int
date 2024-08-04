84cd708b6a8efa018e1c5613d1e7aad7d6ce7dba
Bogdan Nurgatin <BQNEX@outlook.com>
2024-08-04 15:21:07 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+
+  const strArray = String(n).split('')
+
+  if (strArray[0] === '-') {
+    strArray[0] = ''
+    const reversedNumber = strArray.reverse().join('')
+    return reversedNumber
+  } else {
+    const reversedNumber = strArray.reverse().join('')
+    return reversedNumber
+  }
 }
